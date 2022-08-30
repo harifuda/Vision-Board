70aa8fc5c1a490ae0cb00f06c83d45a0fc4a0529
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-08-30 10:06:09 -0400

    Initial Commit

---
+++ VisionBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisionBoard
+//
+//  Created by Lukas Nguyen on 2022-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisionBoard/VisionBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardApp.swift
+//  VisionBoard
+//
+//  Created by Lukas Nguyen on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
